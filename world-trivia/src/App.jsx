87d28d9cd8b1a9523eb216354d4fb6f190cb38eb
KathrinKath <kathrin.peled@dci-student.org>
2024-10-01 12:34:12 +0200

    Animation added - Fardin

--- world-trivia/src/App.jsx
+++ world-trivia/src/App.jsx
@@ -1,16 +1,213 @@
-// src/App.jsx
 import React, { useState } from "react";
+import confetti from "canvas-confetti"; // Import the confetti library
 import { countriesData } from "./Data/countriesData";
+import "./App.css";
+import "font-awesome/css/font-awesome.min.css"; // Import Font Awesome
+import Navbar from "../src/Components/navbar"; // Import the Navbar component
 
-const App = () => {
- 
-  };
+function App() {
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+  const [userAnswer, setUserAnswer] = useState("");
+  const [clicked, setClicked] = useState(false);
+
+  const countries = Object.keys(countriesData);
+
+  function handleCountryChange(e) {
+    setSelectedCountry(e.target.value);
+    setCurrentQuestionIndex(0);
+    setScore(0);
+    setShowResult(false);
+    setUserAnswer("");
+    setClicked(false);
+  }
+
+  function handleAnswerClick(option) {
+    setClicked(true);
+
+    if (
+      option ===
+      countriesData[selectedCountry].questions[currentQuestionIndex]
+        .correctAnswer
+    ) {
+      setScore(score + 1);
+      triggerConfetti(); // Trigger confetti on correct answer
+    }
+    setUserAnswer(option);
+
+    setTimeout(function () {
+      setUserAnswer("");
+      setClicked(false);
+
+      if (
+        currentQuestionIndex <
+        countriesData[selectedCountry].questions.length - 1
+      ) {
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+      } else {
+        setShowResult(true);
+      }
+    }, 10000); // 10 seconds delay so he user can see the correct answers explanation.
+  }
+
+  function triggerConfetti() {
+    const canvas = document.createElement("canvas");
+    const container = document.getElementById("confetti-container");
+    canvas.width = 600;
+    canvas.height = 600;
+
+    container.appendChild(canvas);
+
+    const confettiInstance = confetti.create(canvas, {
+      resize: true,
+      useWorker: true
+    });
+
+    confettiInstance({
+      spread: 160,
+      startVelocity: 30,
+      ticks: 60,
+      gravity: 0.3,
+      colors: [
+        "#ff0",
+        "#0f0",
+        "#00f",
+        "#f00",
+        "#ff00ff",
+        "#00ffff",
+        "#ff0",
+        "#0f0",
+        "#00f",
+        "#f00",
+        "#ff00ff",
+        "#00ffff"
+      ]
+    });
+
+    setTimeout(() => {
+      container.removeChild(canvas);
+    }, 2000); // Remove canvas after 2 seconds
+  }
+
+  function handleRestart() {
+    setSelectedCountry("");
+    setCurrentQuestionIndex(0);
+    setScore(0);
+    setShowResult(false);
+    setClicked(false);
+  }
 
   return (
     <div>
-     
+      {/* Navbar component */}
+      <Navbar />
+      <label className="selectCountryLabel">Select a Country: </label>
+      <select
+        onChange={handleCountryChange}
+        value={selectedCountry}
+        className="selectCountry"
+      >
+        <option value="" className="selectCountryOptions">
+          ***Choose a Country***
+        </option>
+        {countries.map(function (country) {
+          return (
+            <option key={country} value={country}>
+              {country.replace(/_/g, " ")}
+            </option>
+          );
+        })}
+      </select>
+      <div id="confetti-container" style={{ position: "relative" }}></div>{" "}
+      {/* Container for confetti */}
+      {selectedCountry && !showResult && (
+        <div>
+          <h2 className="chosenCountry">
+            {selectedCountry.replace(/_/g, " ")} Trivia
+          </h2>
+
+          {/* Real-time score display */}
+          <h3 className="scoreTitle">Score: {score}</h3>
+
+          {/* Real-time progress display */}
+          <h3 className="questionsBankNumber">
+            Question {currentQuestionIndex + 1} of{" "}
+            {countriesData[selectedCountry].questions.length}
+          </h3>
+
+          <h3 className="fullQuestion">
+            Question {currentQuestionIndex + 1}:{" "}
+            {
+              countriesData[selectedCountry].questions[currentQuestionIndex]
+                .question
+            }
+          </h3>
+          <div className="options-grid">
+            {countriesData[selectedCountry].questions[
+              currentQuestionIndex
+            ].options.map(function (option) {
+              const isCorrect =
+                option ===
+                countriesData[selectedCountry].questions[currentQuestionIndex]
+                  .correctAnswer;
+
+              // Determine the class to apply based on the answer state
+              const buttonClass =
+                clicked && userAnswer
+                  ? isCorrect
+                    ? "correct-answer"
+                    : "incorrect-answer"
+                  : "";
+
+              return (
+                <button
+                  key={option}
+                  onClick={function () {
+                    handleAnswerClick(option);
+                  }}
+                  className={buttonClass} // Use className instead of style
+                  disabled={clicked}
+                >
+                  {option}
+                </button>
+              );
+            })}
+          </div>
+          {userAnswer && (
+            <div>
+              <p className="isCorrectAnswer">
+                {userAnswer ===
+                countriesData[selectedCountry].questions[currentQuestionIndex]
+                  .correctAnswer
+                  ? "Correct!"
+                  : "Wrong!"}
+              </p>
+              <p className="answerExplanation">
+                Explanation:{" "}
+                {
+                  countriesData[selectedCountry].questions[currentQuestionIndex]
+                    .explanation
+                }
+              </p>
+            </div>
+          )}
+        </div>
+      )}
+      {showResult && (
+        <div>
+          <h2 className="finalScoresTitle">
+            Your Score: {score} /{" "}
+            {countriesData[selectedCountry].questions.length}
+          </h2>
+          <button onClick={handleRestart}>
+            <i className="fa fa-refresh"></i> Play Again
+          </button>
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default App;
---
+++ world-trivia/src/Components/navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo-container">
+        <img src="src/images/logo.gif" alt="logo " className="logo" />
+      </div>
+      <h1 className="navbar-title">Trivia Game</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
